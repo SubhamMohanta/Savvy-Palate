1bcc5e5e21850ce40ad7c2db6d6c745d872375ec
SubhamMohanta <subhammohanta99@gmail.com>
2023-12-01 17:43:57 +0530

    Second-commit

--- app.js
+++ app.js
@@ -1,30 +1,9 @@
-var home = document.getElementById("Home");
-var special = document.getElementById("Special");
-var about = document.getElementById("About");
-var contact = document.getElementById("Contact");
-
-
 const randomCard=document.getElementById("random-food")
 const footerRandomCard=document.getElementById("footer-random-food")
 const ingredeintsCard=document.getElementById("ing")
 const recipebutton=document.querySelector(".recipe-btn")
 const recipeCard=document.getElementById("texting")
 
-
-home.onclick=()=>{
-    
-}
-
-special.onclick=()=>{
-
-}
-about.onclick=()=>{
-    
-}
-contact.onclick=()=>{
-    
-}
-// creating onclick events to show ingredients on the card 
 randomCard.onclick=()=>{
     ingredeintsCard.classList.remove("hide")
 }
@@ -32,7 +11,6 @@ ingredeintsCard.onclick=()=>{
     ingredeintsCard.classList.add("hide")
 }
 
-// Function to show random food of the day
 function randomFood(){
     fetch("https://www.themealdb.com/api/json/v1/1/random.php")
     .then((data)=>{
@@ -40,7 +18,6 @@ function randomFood(){
     }).then((completeData)=>{
         let random='';
         let random2='';
-        //adding food card details in the card
         random=`<div class="food-card">
             <div id="food-card-inner">                    
                 <div>
@@ -55,7 +32,6 @@ function randomFood(){
                 
             </div>
         </div>`
-        //adding ingredients details in the ingredients card
         random2=`<div class="ingredeints">
                     <h3>${completeData.meals[0].strMeal}</h3>
                     <h3>Category</h3>
@@ -72,18 +48,18 @@ function randomFood(){
                     ${completeData.meals[0].strIngredient9} <br>
                     ${completeData.meals[0].strIngredient10}</p>
                 </div>`;
-    //replacing them from the arrays 
+
     document.getElementById("random-food").innerHTML=random;
     document.getElementById("ing").innerHTML=random2;
-    // code for showing if there is some error 
+
     }).catch((error)=>{
         console.log(error)
     })
 }
-// calling randomfood function 
+
 randomFood()
 
-// function for searching dish category
+
 var search=document.getElementById("searchBox")
 function SearchedFood(){
     document.getElementById("searchheading").classList.remove("hide")
@@ -97,11 +73,18 @@ function SearchedFood(){
         if(completeData.meals){
                 completeData.meals.forEach((meal)=>{
                 random+=`<div class="food-card" data-id="${meal.idMeal}">
+                <div id="food-card-inner">  
                 <div>
-                    <img id="generated"src=${meal.strMealThumb} alt="">
+                    <img id="generated-2"src=${meal.strMealThumb} alt="">
                 </div>
-                <h3>${meal.strMeal}</h3>
-                <p id="recipe-btn" onClick="getMealRecipe(${meal.idMeal})">Get Recipe</p>
+                <div id="real-image">
+                <img id="generated-3"src=${meal.strMealThumb} alt="">
+                </div>
+                <div id="random-meal-text">
+                <h2>${meal.strMeal}</h2>
+                </div>
+                <h3 id="recipe-btn" onClick="getMealRecipe(${meal.idMeal})">Get Recipe</h3>
+                </div>
                 </div>`
         })
         }
@@ -114,7 +97,7 @@ function SearchedFood(){
     })
     }
 
-    // display ingredients after clicking on searched category 
+
     function getMealRecipe(e){
             fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i="+e)
             .then(response => response.json())
@@ -138,7 +121,7 @@ function SearchedFood(){
                 }
             });
         }
-// Code for making the enter keyword functionable 
+
 document.getElementById("searchBox").addEventListener("keydown",(e)=>{
     if(e.code=="Enter"){
         SearchedFood()
